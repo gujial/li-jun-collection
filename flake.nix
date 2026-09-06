3f84f8f848eb53577a277c9c65f6532108cfa5d1
gujial <gujial@gujial.cc>
2026-09-06 22:42:43 +0800

    update

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Development environment for li-jun-collection";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_22
+              yarn
+            ];
+          };
+        }
+      );
+    };
+}
