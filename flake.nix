a1ca90df32444987da43fc65e1f6518b8aa32a6f
gujial <gujial@gujial.cc>
2026-09-07 11:00:17 +0800

    update yarn

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           default = pkgs.mkShell {
             packages = with pkgs; [
               nodejs_22
-              yarn
+              corepack
             ];
           };
         }
